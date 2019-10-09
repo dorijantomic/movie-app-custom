7e98745323454e7965e83e2775a70845d07b1ff9
Dorijan Tomić <dorijan11@gmail.com>
2019-10-09 17:03:22 +0200

    created button group

--- src/containers/MovieListContainer/MovieListContainer.js
+++ src/containers/MovieListContainer/MovieListContainer.js
@@ -1,25 +1,22 @@
 import React, { Component } from "react";
-import './MovieListContainer.scss'
+import "./MovieListContainer.scss";
 import Card from "../../components/Card/Card";
-
+import ButtonGroup from "../../components/ButtonGroup/ButtonGroup";
 
 class MovieListContainer extends Component {
   state = {
     movies: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
   };
   render() {
- 
-    
     return (
-     
-        <div className="movie-list">
-          {this.state.movies.map((movie, i) => {
-            return <Card />;
-          })}
-        </div>
-    
+      <div className="movie-list">
+        {this.state.movies.map((movie, i) => {
+          return <Card />;
+        })}
+        <ButtonGroup />
+      </div>
     );
   }
 }
 
-export default MovieListContainer
+export default MovieListContainer;
