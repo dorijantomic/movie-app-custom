128721fdee6d7bde6560e04569caf8e176bb9df3
Dorijan Tomić <dorijan11@gmail.com>
2019-10-09 19:48:27 +0200

    Updates

---
+++ precache-manifest.2f6269f056884fbca200b0dd7d7b621f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43101c449b296e16b5c2fba6fa86b7ba",
+    "url": "/movie-app-custom/index.html"
+  },
+  {
+    "revision": "b3f32eb450e723fb6f94",
+    "url": "/movie-app-custom/static/css/main.a4a0403f.chunk.css"
+  },
+  {
+    "revision": "c592751e50f2ee3dd426",
+    "url": "/movie-app-custom/static/js/2.1eed5add.chunk.js"
+  },
+  {
+    "revision": "b3f32eb450e723fb6f94",
+    "url": "/movie-app-custom/static/js/main.b8118899.chunk.js"
+  },
+  {
+    "revision": "4c45da141f3ac9d48ccd",
+    "url": "/movie-app-custom/static/js/runtime-main.e31dd655.js"
+  }
+]);
