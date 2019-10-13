69897bdb701ab9671e2dcdd4d1526a2b060c610f
Dorijan Tomić <dorijan11@gmail.com>
2019-10-13 14:19:55 +0200

    only the release year visible on card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,29 +2,33 @@ import React, { Fragment } from "react";
 import "./Card.scss";
 
 const Card = ({ moviesList }) => {
-  console.log(moviesList, "the fuck")
+  console.log(moviesList, "the fuck");
   return (
     <Fragment>
-      {moviesList!== null ? moviesList.map((movie, i) => (
-       
-         <div className="card-container" key={movie.id}>
-         <div className="card-container__rating">
-           <span>{movie.vote_average}</span>
-         </div>
-         <div className="card-container__img">
-           <img
-             src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`}
-             alt=""
-             srcset=""
-           />
-         </div>
-         <div className="card-container__text">
-           <h1>{movie.title} ({movie.release_date})</h1>
-           <p>Language: {movie.original_language}</p>
-         </div>
-       </div>
-      )): <h1>Loading...</h1>}
-    
+      {moviesList !== null ? (
+        moviesList.map((movie, i) => (
+          <div className="card-container" key={movie.id}>
+            <div className="card-container__rating">
+              <span>{movie.vote_average}</span>
+            </div>
+            <div className="card-container__img">
+              <img
+                src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`}
+                alt=""
+                srcset=""
+              />
+            </div>
+            <div className="card-container__text">
+              <h1>
+                {movie.title} ({movie.release_date.slice(0, 4)})
+              </h1>
+              <p>Language: {movie.original_language}</p>
+            </div>
+          </div>
+        ))
+      ) : (
+        <h1>Loading...</h1>
+      )}
     </Fragment>
   );
 };
