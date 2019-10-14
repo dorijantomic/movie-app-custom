1192f53c97e95273289b45e0c2314a139e6f8045
Dorijan Tomić <dorijan11@gmail.com>
2019-10-14 16:08:51 +0200

    routing for movie details basic done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 
 import "./App.scss";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import MovieListContainer from "./containers/MovieListContainer/MovieListContainer";
+import MovieDetailsContainer from "./containers/MovieDetailsContainer/MovieDetailsContainer";
 
 function App() {
-  return <MovieListContainer />;
+  return (
+    <HashRouter>
+      <Switch>
+        <Route path="/" exact component={MovieListContainer} />
+        <Route path="/:movie" exact component={MovieDetailsContainer} />
+      </Switch>
+    </HashRouter>
+  );
 }
 
 export default App;
--- src/containers/MovieDetailsContainer/MovieDetailsContainer.js
+++ src/containers/MovieDetailsContainer/MovieDetailsContainer.js
@@ -4,7 +4,7 @@ export default class MovieDetailsContainer extends Component {
     render() {
         return (
             <div>
-                
+                <h1 style={{color: "white"}}>Hello You are on movie details</h1>
             </div>
         )
     }
